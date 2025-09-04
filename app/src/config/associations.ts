737b3bbfa8ae8737a7091cbf77a3515baf94a76c
takanopontaro <takano@superhero.jp>
2025-09-04 21:02:04 +0900

    AssociationsConfigのサンプルを、コメントとして記述する

--- app/src/config/associations.ts
+++ app/src/config/associations.ts
@@ -1,8 +1,13 @@
 import type { AssociationsConfig } from '@modules/App/types';
 
 const associations: AssociationsConfig = [
+  // By MIME type
   // { kind: 'mime', pattern: /^text\//, app: 'Visual Studio Code' },
+  //
+  // By file path
   // { kind: 'path', pattern: /\.json$/, app: 'Visual Studio Code' },
+  //
+  // By function
   // (mime, path) => {
   //   if (mime === 'application/toml') {
   //     return 'Visual Studio Code';
