b1ddb3effec6b873cdc865a7a61a4d7d2e054e5a
takanopontaro <takano@superhero.jp>
2025-09-09 21:16:39 +0900

    デフォルトstoreではなく自前のstoreを使うよう変更し、configを初期値として注入する

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,14 +1,20 @@
+import { Provider } from 'jotai';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { store } from '@libs/store';
 import { App } from '@modules/App/components/App';
+import { $config } from '@modules/App/state';
 
 import type { Config } from '@modules/App/types';
 
 function init(config: Config): void {
+  store.set($config, config);
   const root = document.getElementById('root')!;
   createRoot(root).render(
     <StrictMode>
-      <App config={config} />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>,
   );
 }
--- app/src/modules/App/components/App.tsx
+++ app/src/modules/App/components/App.tsx
@@ -1,27 +1,19 @@
-import { useAtomValue, useSetAtom } from 'jotai';
+import { useAtomValue } from 'jotai';
 import { memo } from 'react';
 import { createPortal } from 'react-dom';
 import { useInitialDir, useWebSocket } from '@modules/App/hooks';
-import { $config, $modal } from '@modules/App/state';
+import { $modal } from '@modules/App/state';
 import { DataFrame } from '@modules/DataFrame/components';
 import { LogFrame } from '@modules/LogFrame/components';
 
 import type { FC } from 'react';
-import type { Config } from '@modules/App/types';
 
-type Props = {
-  config: Config;
-};
-
-const AppComponent: FC<Props> = ({ config }) => {
+const AppComponent: FC = () => {
   const wsAtom = useWebSocket(3000);
   const [dirPathA, dirPathB] = useInitialDir();
 
-  const setConfig = useSetAtom($config);
   const loadable = useAtomValue(wsAtom);
   const Modal = useAtomValue($modal);
-
-  setConfig(config);
 
   switch (loadable.state) {
     case 'hasError':
