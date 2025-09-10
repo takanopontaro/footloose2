f863f11247116e72246eba5018aeb5d793fb51cb
takanopontaro <takano@superhero.jp>
2025-09-10 22:41:46 +0900

    refactor: App/state/scope.ts

--- app/src/modules/App/state/scope.ts
+++ app/src/modules/App/state/scope.ts
@@ -1,5 +1,6 @@
 import { atom } from 'jotai';
 import Mousetrap from 'mousetrap';
+import { readState } from '@libs/utils';
 import { $activeFrame, $api, $config, $modes, $tags } from '@modules/App/state';
 
 import type { SetStateAction } from 'jotai';
@@ -11,20 +12,53 @@ import type {
   Tag,
 } from '@modules/App/types';
 
-type ShortcutAction = Omit<ShortcutCommand, 'cmd'> & {
+type ActionsConfig = Omit<ShortcutCommand, 'cmd'> & {
   action: CommandAction;
 };
 
-function isShortcutAction(item: unknown): item is ShortcutAction {
-  return item !== null;
+// コマンドオブジェクトを元に、そのコマンドの関数本体を取得し、
+// 元の設定と組み合わせて返す。
+function getActionsConfigList(
+  shortcutCommandList: ShortcutCommand[],
+): ActionsConfig[] {
+  const { commands } = readState($config);
+  const res: ActionsConfig[] = [];
+  for (const { args, cmd, modes, tags } of shortcutCommandList) {
+    const action = commands.find((v) => v.name === cmd)?.action;
+    if (action) {
+      res.push({ action, args, modes, tags });
+    }
+  }
+  return res;
 }
 
-function isMatch<T>(value: string, curValues: T[]): boolean {
+// その値が集合に含まれるかを判定する。
+// 先頭に ! が付いている場合は、含まれないことを判定する。
+function match<T>(value: string, curValues: T[]): boolean {
   if (value.startsWith('!')) {
     const v = value.slice(1);
     return !curValues.includes(v as T);
   }
   return curValues.includes(value as T);
+}
+
+// 現在の mode や tag に基づいた、適切なコマンドを取得する関数を返す。
+function makeGetAvailableActions(
+  shortcutCommandList: ShortcutCommand[],
+): () => ActionsConfig[] {
+  const configs = getActionsConfigList(shortcutCommandList);
+  return () => {
+    const frame = readState($activeFrame);
+    const curModes = readState($modes(frame));
+    const curTags = readState($tags);
+    return configs
+      .map((c) => {
+        const isInMode = c.modes?.some((m) => match<Mode>(m, curModes));
+        const hasTag = c.tags?.some((t) => match<Tag>(t, curTags));
+        return isInMode !== false && hasTag !== false ? c : null;
+      })
+      .filter((c) => c !== null);
+  };
 }
 
 const scopeAtom = atom<Scope>('');
@@ -41,40 +75,28 @@ export const $scope = atom(
     }
     set(scopeAtom, newVal);
 
+    // ------------------------------------
+    // scope に紐づいたショートカットキーを設定する。
+
     Mousetrap.reset();
+
     const api = get($api);
-    const { commands, shortcuts } = get($config);
-    const bindings = shortcuts[newVal] ?? {};
-    Object.entries(bindings).forEach(([key, cmdList]) => {
-      const actionMap = cmdList.reduce<ShortcutAction[]>(
-        (map, { args, cmd, modes, tags }) => {
-          const fn = commands.find((v) => v.name === cmd)?.action;
-          if (fn) {
-            map.push({ action: fn, args, modes, tags });
-          }
-          return map;
-        },
-        [],
-      );
+    const bindings = get($config).shortcuts[newVal] ?? {};
+
+    for (const [key, shortcutCommandList] of Object.entries(bindings)) {
+      const getAvailableActions = makeGetAvailableActions(shortcutCommandList);
       Mousetrap.bind(key, (_e, combo) => {
-        const frame = get($activeFrame);
-        const curModes = get($modes(frame));
-        const curTags = get($tags);
-        const actions = actionMap
-          .map((map) => {
-            const isInMode = map.modes?.some((m) => isMatch<Mode>(m, curModes));
-            const hasTag = map.tags?.some((t) => isMatch<Tag>(t, curTags));
-            return isInMode !== false && hasTag !== false ? map : null;
-          })
-          .filter(isShortcutAction);
+        const actions = getAvailableActions();
         (async () => {
           for (const { action, args } of actions) {
             await api.run(action, combo, args);
           }
         })().catch(() => {});
+        // コマンドが実行された場合は false を返し、
+        // イベントの伝播とブラウザのデフォルト挙動を止める。
         const executed = actions.length > 0;
         return !executed;
       });
-    });
+    }
   },
 );
