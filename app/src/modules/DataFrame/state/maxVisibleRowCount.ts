9cb4a66fc7c83b11a73f0cc236a7a30f18eecded
takanopontaro <takano@superhero.jp>
2025-09-25 21:38:16 +0900

    refactor: modules/DataFrame/state/maxVisibleRowCount.ts

--- app/src/modules/DataFrame/state/maxVisibleRowCount.ts
+++ app/src/modules/DataFrame/state/maxVisibleRowCount.ts
@@ -3,6 +3,6 @@ import { atomFamily } from 'jotai/utils';
 
 import type { Frame } from '@modules/App/types';
 
-// 初回読込時は、値の算出のため dummyEntry を描画する。
+// 初回読込時は、値の算出のためダミーのエントリーを描画したい。
 // そのため 1 を初期値とする。
 export const $maxVisibleRowCount = atomFamily((_frame: Frame) => atom(1));
