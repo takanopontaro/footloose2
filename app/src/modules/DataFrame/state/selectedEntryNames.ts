abe2d8035c15e4e27752a48c973e6aa39f35a298
takanopontaro <takano@superhero.jp>
2025-09-26 21:17:19 +0900

    refactor: modules/DataFrame/state/selectedEntryNames.ts

--- app/src/modules/DataFrame/state/selectedEntryNames.ts
+++ app/src/modules/DataFrame/state/selectedEntryNames.ts
@@ -1,6 +1,7 @@
 import { atom } from 'jotai';
 import { RESET, atomFamily, atomWithReset } from 'jotai/utils';
 import { shallowEqualArray } from '@libs/utils';
+import { $filteredEntries } from '@modules/DataFrame/state';
 
 import type { SetStateAction } from 'jotai';
 import type { Frame } from '@modules/App/types';
@@ -21,13 +22,25 @@ export const $selectedEntryNames = atomFamily((frame: Frame) =>
         set(selectedEntryNamesAtom(frame), RESET);
         return;
       }
-      // `..` は選択できないようにする。空文字も弾く。
+
+      // `..` は選択できないようにし、空文字も弾く。
       const names = [...new Set(newVal)]
         .sort()
         .filter((n) => n !== '' && n !== '..');
+
       if (shallowEqualArray(names, curVal)) {
         return;
       }
+
+      const entries = get($filteredEntries(frame));
+      const entryNames = new Set(entries.map((e) => e.name));
+
+      // 無効な値があれば、更新せず return する。
+      const hasInvalid = names.some((n) => !entryNames.has(n));
+      if (hasInvalid) {
+        return;
+      }
+
       set(selectedEntryNamesAtom(frame), names);
     },
   ),
