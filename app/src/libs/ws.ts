82b8c9b840a70960eaa88c99a7233eefb9d8d22e
takanopontaro <takano@superhero.jp>
2025-10-01 22:31:50 +0900

    refactor: libs/ws.ts

--- app/src/libs/ws.ts
+++ app/src/libs/ws.ts
@@ -23,32 +23,39 @@ const LISTENER_STATUS = {
   watchError: 'WATCH_ERROR',
 } as const;
 
+// 型ガード
+// リスナー型のレスポンスかどうかを返す。
 function isListenerResp(resp: WsResponse): resp is WsListenerResponse {
   const values = Object.values(LISTENER_STATUS) as readonly string[];
   return values.includes(resp.status);
 }
 
 class Ws {
+  // WebSocket のインスタンス
   #ws: WebSocket | null = null;
 
-  // watch などの常時監視用途
+  // リスナー
+  // 継続的に監視するもの (watch など)
   #listeners: {
     [K in keyof ListenerRespMap]?: ((resp: ListenerRespMap[K]) => void)[];
   } = {};
 
-  // command などの単発系用途
+  // コールバック
+  // 一度限りの単発もの (command など)
   #callbacks = new Map<string, (resp: unknown) => void>();
 
   init(ws: WebSocket): Ws {
     this.#ws = ws;
     ws.addEventListener('message', (e: MessageEvent<string>) => {
       const resp = JSON.parse(e.data) as WsResponse;
+      // リスナーを実行する。
       if (isListenerResp(resp)) {
         this.#execListener(resp.status, resp);
         return;
       }
+      // コールバックを実行し、消去する。
       const fn = this.#callbacks.get(resp.cid);
-      if (fn !== undefined) {
+      if (fn) {
         this.#callbacks.delete(resp.cid);
         fn(resp);
       }
@@ -56,21 +63,60 @@ class Ws {
     return this;
   }
 
+  // リスナーを登録する。
+  registerListener<S extends keyof ListenerRespMap>(
+    status: S,
+    listener: (resp: ListenerRespMap[S]) => void,
+  ): void {
+    if (!this.#ws) {
+      throw new Error('WebSocket is not initialized');
+    }
+    // こう書きたかったが、[] が never[] 型になってしまうため断念した。
+    // const arr = this.#listeners[status] ?? [];
+    // arr.push(listener);
+    if (!this.#listeners[status]) {
+      this.#listeners[status] = [];
+    }
+    this.#listeners[status].push(listener);
+  }
+
+  // リスナーを実行する。
   #execListener<S extends keyof ListenerRespMap>(
     status: S,
     resp: ListenerRespMap[S],
   ): void {
     const listeners = this.#listeners[status];
-    if (listeners !== undefined) {
+    if (listeners) {
       listeners.forEach((fn) => fn(resp));
     }
   }
 
+  // リスナーを削除する。
+  removeListener<S extends keyof ListenerRespMap>(
+    status: S,
+    listener: (resp: ListenerRespMap[S]) => void,
+  ): void {
+    if (!this.#ws) {
+      throw new Error('WebSocket is not initialized');
+    }
+    const arr = this.#listeners[status];
+    if (!arr) {
+      return;
+    }
+    const index = arr.findIndex((l) => l === listener) ?? -1;
+    if (index !== -1) {
+      arr.splice(index, 1);
+    }
+  }
+
+  // WebSocket サーバーにデータを送信する。
+  // id を発行し、それをキーとしてコールバックを登録してから送る。
+  // 受信時、その id がレスポンスに含まれているため、コールバックを取得できる。
   send<R extends WsResponse>(
     data: Omit<WsCommand, 'id'>,
     callback: WsSendCallback<R>,
   ): void {
-    if (this.#ws === null) {
+    if (!this.#ws) {
       throw new Error('WebSocket is not initialized');
     }
     const id = crypto.randomUUID();
@@ -78,36 +124,6 @@ class Ws {
     this.#callbacks.set(id, callback as (resp: unknown) => void);
     const msg = JSON.stringify({ ...data, id });
     this.#ws.send(msg);
-  }
-
-  registerListener<S extends keyof ListenerRespMap>(
-    status: S,
-    listener: (resp: ListenerRespMap[S]) => void,
-  ): void {
-    if (this.#ws === null) {
-      throw new Error('WebSocket is not initialized');
-    }
-    // こう書きたかったが、[] が never[] 型になってしまうため断念した。
-    // const arr = this.#listeners[status] ?? [];
-    // arr.push(listener);
-    if (this.#listeners[status] === undefined) {
-      this.#listeners[status] = [];
-    }
-    this.#listeners[status].push(listener);
-  }
-
-  removeListener<S extends keyof ListenerRespMap>(
-    status: S,
-    listener: (resp: ListenerRespMap[S]) => void,
-  ): void {
-    if (this.#ws === null) {
-      throw new Error('WebSocket is not initialized');
-    }
-    const arr = this.#listeners[status];
-    const index = arr?.findIndex((l) => l === listener) ?? -1;
-    if (index !== -1) {
-      arr?.splice(index, 1);
-    }
   }
 }
 
