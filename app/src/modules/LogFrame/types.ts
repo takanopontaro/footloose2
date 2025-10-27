a0c14845f465b58716c7176ee5664b9ed078a9a4
takanopontaro <takano@superhero.jp>
2025-10-27 21:00:49 +0900

    refactor: modules/LogFrame/types.ts

--- app/src/modules/LogFrame/types.ts
+++ app/src/modules/LogFrame/types.ts
@@ -1,24 +1,67 @@
 import type { ComponentProps, ReactElement, ReactNode } from 'react';
 import type { ProgressTaskLog } from '@modules/LogFrame/components';
 
+/**
+ * ログレベル。
+ */
 export type LogLevel = 'error' | 'info' | 'none' | 'progress' | 'warn';
 
+/**
+ * ログデータ。
+ */
 export type LogData = {
+  /**
+   * ログレベル。
+   */
   level: LogLevel;
+  /**
+   * ログの内容。
+   */
   log: ReactNode;
+  /**
+   * ユニーク ID。
+   */
   uid: string;
 };
 
+/**
+ * ProgressTask のログデータ。
+ */
 export type ProgressTaskLogData = {
+  /**
+   * ログレベル。
+   */
   level: 'progress';
+  /**
+   * ログの内容。
+   * ProgressTaskLog コンポーネントに限定される。
+   */
   log: ReactElement<ComponentProps<typeof ProgressTaskLog>>;
+  /**
+   * ユニーク ID。
+   */
   uid: string;
 };
 
+/**
+ * ProgressTask のステータス。
+ */
 export type ProgressTaskStatus = 'abort' | 'end' | 'error' | 'progress';
 
+/**
+ * ProgressTask の詳細情報。
+ */
 export type ProgressTaskInfo = {
+  /**
+   * プロセス ID。
+   */
   pid: string;
+  /**
+   * 進捗率 (百分率)。
+   */
   progress: number;
+  /**
+   * ステータス。
+   */
   status: ProgressTaskStatus;
 };
