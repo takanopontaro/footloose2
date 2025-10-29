390c9a16cd5512106b707648f23d741c41df3dd8
takanopontaro <takano@superhero.jp>
2025-10-29 21:46:05 +0900

    カレントディレクトリが変わるたびにアプリ全体がre-renderされていた問題を修正する

--- app/src/modules/App/hooks/useInitialDir.ts
+++ app/src/modules/App/hooks/useInitialDir.ts
@@ -1,4 +1,5 @@
 import { useAtomValue } from 'jotai';
+import { useState } from 'react';
 import { $prevSessionDir } from '@modules/App/state';
 
 /**
@@ -11,5 +12,13 @@ import { $prevSessionDir } from '@modules/App/state';
 export const useInitialDir = (): [string, string] => {
   const dirPathA = useAtomValue($prevSessionDir('a'));
   const dirPathB = useAtomValue($prevSessionDir('b'));
-  return [dirPathA, dirPathB];
+
+  // $prevSessionDir はカレントディレクトリが変わるたびに更新されるため、
+  // dirPathA/B (=initialDir) をそのまま返すと re-render のトリガーになってしまう。
+  // useState の初期値は、一度セットすると他の値をセットしても更新されないため、
+  // この仕組みを利用して re-render を防ぐ。
+  const [pathA] = useState(dirPathA);
+  const [pathB] = useState(dirPathB);
+
+  return [pathA, pathB];
 };
