3b4e4fa3f1dd06c5e69c98a7a1e2cfedc1e4e411
takanopontaro <takano@superhero.jp>
2025-11-04 23:00:42 +0900

    不要なフィルタ処理を削除する

--- app/src/modules/DataFrame/state/filterQuery.ts
+++ app/src/modules/DataFrame/state/filterQuery.ts
@@ -3,40 +3,33 @@ import { RESET, atomFamily, atomWithReset } from 'jotai/utils';
 import { readState, writeState } from '@libs/utils';
 import { $modes } from '@modules/App/state';
 import {
-  $activeEntryName,
   $filteredEntries,
   $gridColumnCount,
   $maxVisibleRowCount,
   $firstVisibleEntryIndex,
   $selectedEntryNames,
+  $activeEntryIndex,
 } from '@modules/DataFrame/state';
 
 import type { SetStateAction } from 'jotai';
 import type { Frame } from '@modules/App/types';
-import type { Entry } from '@modules/DataFrame/types';
 
 /**
  * エントリ一覧の filter-out 状況に応じて、表示領域内の開始エントリを更新する。
  *
- * @param entries - エントリ一覧
  * @param frame - 対象フレーム
  */
-function updateFirstVisibleEntryIndex(entries: Entry[], frame: Frame): void {
-  const activeEntryName = readState($activeEntryName(frame));
+function updateFirstVisibleEntryIndex(frame: Frame): void {
+  const activeEntryIndex = readState($activeEntryIndex(frame));
   const gridColumnCount = readState($gridColumnCount(frame));
   const maxRowCount = readState($maxVisibleRowCount(frame));
 
-  // $activeEntryIndex を使いたいところだが、
-  // この atom は $filteredEntries を内部的に参照しているため、
-  // この時点ではまだ正確なインデックスを得られない。
-  // 引数の entries には $filterQuery が反映されているが、
-  // $filteredEntries にはまだ未反映なためである。
-  // よって findIndex する必要がある。
-  const curIndex = entries.findIndex((e) => e.name === activeEntryName);
-
   // カレントエントリが filter-out されている、または
   // スクロール無しで全エントリを表示できる場合。
-  if (curIndex === -1 || curIndex < maxRowCount * gridColumnCount) {
+  if (
+    activeEntryIndex === -1 ||
+    activeEntryIndex < maxRowCount * gridColumnCount
+  ) {
     writeState($firstVisibleEntryIndex(frame), 0);
     return;
   }
@@ -49,7 +42,7 @@ function updateFirstVisibleEntryIndex(entries: Entry[], frame: Frame): void {
   const halfEntryCount = Math.ceil(maxRowCount / 2) * gridColumnCount;
 
   // カーソルが表示領域の中央あたりに来るよう、開始エントリを調整する。
-  let firstEntryIndex = curIndex - halfEntryCount;
+  let firstEntryIndex = activeEntryIndex - halfEntryCount;
 
   // グリッドがズレないように、列数の倍数が先頭インデックスになるよう調整する。
   firstEntryIndex = firstEntryIndex - (firstEntryIndex % gridColumnCount);
@@ -76,8 +69,7 @@ export const $filterQuery = atomFamily((frame: Frame) =>
       if (newVal === RESET || newVal === '') {
         set(filterQueryAtom(frame), RESET);
         set($modes(frame), (prev) => prev.filter((m) => m !== 'filter'));
-        const entries = get($filteredEntries(frame));
-        updateFirstVisibleEntryIndex(entries, frame);
+        updateFirstVisibleEntryIndex(frame);
         return;
       }
 
@@ -95,23 +87,16 @@ export const $filterQuery = atomFamily((frame: Frame) =>
       // read-only atom であり、setter が無いため、ここで行う。
       // (getter 内で atom の更新をしたくない)
 
-      // パターン入力中は容易に不完全な正規表現になり得るため、
-      // try-catch でしっかりガードする。
-      try {
-        const re = new RegExp(newVal, 'i');
-        let entries = get($filteredEntries(frame));
-        entries = entries.filter((e) => re.test(e.name));
-        updateFirstVisibleEntryIndex(entries, frame);
+      // ここで得られるエントリ一覧にはすでに最新のフィルタが反映されている。
+      const entries = get($filteredEntries(frame));
 
-        // filter-out される (だろう) エントリを非選択にする。
-        // (filter-out 自体は $filteredEntries で行われる)
-        const entryNames = new Set(entries.map((e) => e.name));
-        set($selectedEntryNames(frame), (prev) =>
-          prev.filter((n) => entryNames.has(n)),
-        );
-      } catch (_e) {
-        // 握りつぶす ✊💥
-      }
+      updateFirstVisibleEntryIndex(frame);
+
+      // filter-out されたエントリを非選択状態にする。
+      const entryNames = new Set(entries.map((e) => e.name));
+      set($selectedEntryNames(frame), (prev) =>
+        prev.filter((n) => entryNames.has(n)),
+      );
     },
   ),
 );
