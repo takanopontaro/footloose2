e7fc1f68eb700ff734ef6c1049126cad495f79ab
takanopontaro <takano@superhero.jp>
2025-12-18 21:25:28 +0900

    メディアクエリでCSS変数の値が変わった時、atomに反映されるようにする

--- app/src/modules/DataFrame/state/gridColumnCount.ts
+++ app/src/modules/DataFrame/state/gridColumnCount.ts
@@ -1,20 +1,53 @@
 import { atom } from 'jotai';
 import { atomFamily } from 'jotai/utils';
-import { getCssVariable } from '@libs/utils';
+import { debounce, getCssVariable } from '@libs/utils';
 import { $isGalleryMode } from '@modules/DataFrame/state';
 
+import type { SetStateAction } from 'jotai';
 import type { Frame } from '@modules/App/types';
+
+/**
+ * CSS カスタムプロパティで定義されている「グリッドの列数」を取得する。
+ *
+ * @returns グリッドの列数
+ */
+function getGridColumnCount(): number {
+  const n = parseInt(getCssVariable('--grid-column-count'), 10);
+  if (Number.isFinite(n)) {
+    return n;
+  }
+  throw new Error('`--grid-column-count` is not defined in your css');
+}
+
+const gridColumnCountAtom = atomFamily((_frame: Frame) => atom(1));
 
 /**
  * グリッドの列数。
  * リスト表示時は 1 列である。
  */
-export const $gridColumnCount = atomFamily((frame: Frame) =>
-  atom((get) => {
-    const isGalleryMode = get($isGalleryMode(frame));
-    if (isGalleryMode) {
-      return parseInt(getCssVariable('--grid-column-count'));
-    }
-    return 1;
-  }),
-);
+export const $gridColumnCount = atomFamily((frame: Frame) => {
+  const atm = atom(
+    (get) => {
+      const isGalleryMode = get($isGalleryMode(frame));
+      const val = get(gridColumnCountAtom(frame));
+      return isGalleryMode ? val : 1;
+    },
+    // この setter は onMount からのみ呼び出される想定。
+    (_get, set, newVal: SetStateAction<number>) => {
+      set(gridColumnCountAtom(frame), newVal);
+    },
+  );
+
+  atm.onMount = (set) => {
+    const update = (): void => set(getGridColumnCount());
+    update(); // 念のため…
+    const fn = debounce(update, 200);
+    window.addEventListener('resize', fn);
+    return () => {
+      fn.cancel();
+      window.removeEventListener('resize', fn);
+    };
+  };
+
+  return atm;
+});
--- app/src/modules/DataFrame/state/renderedRowHeight.ts
+++ app/src/modules/DataFrame/state/renderedRowHeight.ts
@@ -1,10 +1,37 @@
 import { atom } from 'jotai';
-import { getCssVariable } from '@libs/utils';
+import { debounce, getCssVariable } from '@libs/utils';
+
+/**
+ * CSS カスタムプロパティで定義されている「行の高さ」を取得する。
+ *
+ * @returns 行の高さ
+ */
+function getRowHeight(): number {
+  const n = parseInt(getCssVariable('--row-height'), 10);
+  if (Number.isFinite(n)) {
+    return n;
+  }
+  throw new Error('`--row-height` is not defined in your css');
+}
 
 /**
  * レンダリングされた時の行の高さ。
  * グリッドの表示領域関連の計算に使用される。
+ * なお、この atom の setter は onMount からのみ呼び出される想定。
  */
-export const $renderedRowHeight = atom(() =>
-  parseInt(getCssVariable('--row-height')),
-);
+export const $renderedRowHeight = atom(getRowHeight());
+
+/**
+ * メディアクエリ等で CSS カスタムプロパティの値が動的に変わる可能性があるため、
+ * ウィンドウのリサイズを監視しておく。
+ */
+$renderedRowHeight.onMount = (set) => {
+  const update = (): void => set(getRowHeight());
+  update(); // 念のため…
+  const fn = debounce(update, 200);
+  window.addEventListener('resize', fn);
+  return () => {
+    fn.cancel();
+    window.removeEventListener('resize', fn);
+  };
+};
