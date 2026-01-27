59551007dec905724b6f782b6cfef41ecfbc5ed5
takanopontaro <takano@superhero.jp>
2026-01-27 21:00:47 +0900

    アプリ読み込み時に$migemoを初期化する

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { store } from '@libs/store';
 import { App } from '@modules/App/components/App';
-import { $config } from '@modules/App/state';
+import { $config, $migemo } from '@modules/App/state';
 
 import type { Config } from '@modules/App/types';
 
@@ -25,6 +25,7 @@ const migemo = await fetch('./migemo-compact-dict')
  */
 function init(config: Config): void {
   store.set($config, config);
+  store.set($migemo, migemo);
 
   const root = document.getElementById('root')!;
 
